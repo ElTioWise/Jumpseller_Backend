bc49f13bc7db9c05e4863e56cc8b9324ef764b3a
ignacio <ignaciog@fixlabs.cl>
2023-02-27 18:26:05 -0300

    funcionalidad completa

--- app.js
+++ app.js
@@ -19,34 +19,43 @@ app.post('/api/v1', (req, res) => {
     const customerEmail = req.body.order.customer.email
     products = req.body.order.products
     const preguntar = async () =>{
-        await products.forEach(e => {
+            products.forEach(e => {
             console.log(`${e.name} tiene el id ${e.id}`)
             obtenerCategorías(e.id)
         })
     }
     preguntar()
     setTimeout(() => {
-        // console.log(productsId[0]);
         getValueForId(productsId[0],req.body.order.products)
-        console.log(products[0])
-        createPromotion()
-      }, "3000")
+        products.forEach(delayLoop(createPromotion,1))
+      }, "1000")
     res.send(`funciona`);
 });
+const login = '59c8deed7a6436f0509b31e98cedd508'
+const token = '4515c208fa02f6460a5b775f30519731'
+const urlProducts = 'https://api.jumpseller.com/v1/products/'
+const urlPromotions = 'https://api.jumpseller.com/v1/promotions.json'
 function getValueForId(i,o){
     products.forEach(e => {
         return e.id === productsId[0]
     })
 }
-function createPromotion () {
-    api
-    let dat= new Date();
-    let dat2 = Date.parse(dat);
-    console.log(dat2)
+const delayLoop = (fn, delay) => {
+    return (e, i) => {
+        setTimeout(() => {
+            createPromotion(e)
+        }, i * 1000);
+    }
+}
+function createPromotion (e) {
+    let date= new Date();
+    let dateNr = (Date.parse(date));
+    let dateFinal = Math.floor(dateNr/1000);
+    createGiftcard(e,dateFinal)
 }
 async function obtenerCategorías (e) {
     await api
-        .get(`https://api.jumpseller.com/v1/products/${e}.json?login=59c8deed7a6436f0509b31e98cedd508&authtoken=4515c208fa02f6460a5b775f30519731`)
+        .get(`${urlProducts}${e}.json?login=${login}&authtoken=${token}`)
         .then( res => {
       // handle success
     const data = res.data.product.categories
@@ -67,7 +76,73 @@ async function obtenerCategorías (e) {
         console.log(error)
     })
 }
-// let webhook = req.body.order.subtotal
-// webhook = webhook / 100 * 5
-// console.log(webhook)
-// res.send(`Se creó un cupón de ${webhook} por su compra de ${req.body.order.subtotal},asignada al correo ${req.body.order.customer.email} ,Gracias por usar nuestro sistema de puntos :)`);
+function createGiftcard (data,data2) {
+    console.log(data)
+    console.log(data2)
+    const estructura = {
+        "promotion": {
+            "name": "TestNuevo",
+            "enabled": true,
+            "discount_target": "order",
+            "buys_at_least": "price",
+            "condition_price": 0,
+            "condition_qty": 0,
+            "quantity_x": 0,
+            "type": "fix",
+            "discount_amount_fix": 10000,
+            "discount_amount_percent": 0,
+            "lasts": "none",
+            "begins_at": "string",
+            "expires_at": "string",
+            "max_times_used": 1,
+            "cumulative": false,
+            "customers": "string",
+            "categories": [
+                {
+                    "id": 0
+                }
+            ],
+            "customer_categories": [
+                {
+                    "id": 0
+                }
+            ],
+            "products": [
+                {
+                    "id": 0
+                }
+            ],
+            "products_x": [
+                {
+                    "id": 0
+                }
+            ],
+            "coupons": [
+                {
+                    "code": `${data2}`,
+                    "usage_limit": 1
+                }
+            ],
+            "countries": [
+                "string"
+            ],
+            "regions": [
+                "string"
+            ],
+            "municipalities": [
+                "string"
+            ]
+        }
+    }
+    api
+        .post(`${urlPromotions}?login=${login}&authtoken=${token}`,estructura)
+        .then(res =>{
+            console.log("funcionó")
+            console.log(res.data)
+        })
+        .catch(err => {
+            if(err.response.status == 400){
+                console.log("Numero repetido")
+            }
+        })
+}
