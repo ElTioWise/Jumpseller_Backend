4ddee8ce6950733258cd45445742fbcd78e92164
ignacio <ignaciog@fixlabs.cl>
2023-02-27 22:50:55 -0300

    diferencia en cantidades y responde con las gift creadas

--- app.js
+++ app.js
@@ -1,53 +1,83 @@
+// Dependencias requeridas
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const api = require('axios');
+//valida que no estemos usando las variables de entorno de un ambiente de producción
 if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
 }
-
+//Puerto en el que está corriendo
 app.listen(PORT, ()=> console.log(`Servidor corriendo en el puerto ${PORT}`));
-
 app.use(bodyParser.json());
-
+//Variables globales
 const login = process.env.LOGIN;
 const token = process.env.TOKEN;
-const urlProducts = 'https://api.jumpseller.com/v1/products/'
-const urlPromotions = 'https://api.jumpseller.com/v1/promotions.json'
-
-// Route
-app.get('/', (req, res) => {
-    res.send('Hola Mundo!');
-});
+const urlProducts = process.env.URLPRODUCTS
+const urlPromotions = process.env.URLPROMOTIONS
 let productsId = []
 let products = []
+let productCreate = []
+let Giftcard = []
 
+//Ent-point Ruta Gatilladora
 app.post('/api/v1', (req, res) => {
-    const customerEmail = req.body.order.customer.email
-    products = req.body.order.products
-    const preguntar = async () =>{
-            products.forEach(e => {
-            obtenerCategorías(e.id)
-        })
-    }
-    preguntar()
-    setTimeout(() => {
-        getValueForId(productsId[0],req.body.order.products)
-        products.forEach(delayLoop(createPromotion,1))
-      }, "1000")
-    res.send(`funciona`);
-});
-function getValueForId(i,o){
+    //Email del comprador de la orden
+    // const customerEmail = req.body.order.customer.email
+
+    products = req.body.order.products //asignación de todos los productos a variable global
     products.forEach(e => {
-        return e.id === productsId[0]
+        obtenerCategorías(e.id)
+    })
+    //Consulta los ids de los productos que cuentan con la categoría de giftcard
+    
+    setTimeout(() => {
+        filterForId()
+        productCreate.forEach(delayLoop(createPromotion,2))//delay en segundos
+    }, "1000")
+    setTimeout(() => {
+        res.send(`${Giftcard}`);
+    },"5000")
+});
+
+//Funciones
+//Función de validación de categoría
+async function obtenerCategorías (e) {
+    await api
+        .get(`${urlProducts}${e}.json?login=${login}&authtoken=${token}`)
+        .then( res => {
+        // handle success
+        const data = res.data.product.categories
+        // const productName = res.data.product.name
+        const idProduct = res.data.product.id
+        data.forEach(f => {
+            if(f.id == 1439748){
+                // console.log(`el producto ${productName} si cuenta con la categoría de Giftcard`)
+                productsId.push(idProduct)
+                return
+            }
+            else{
+                // console.log(`el producto ${productName} no cuenta con la categoría de Giftcard`)
+            }
+            });
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+}
+function filterForId(){
+    productCreate = products.filter(f => {
+        if(f.id == productsId[0]){
+            return f
+        }
     })
 }
-const delayLoop = (fn, delay) => {
-    return (e, i) => {
+const delayLoop = (e, f) => {
+    return (e, f) => {
         setTimeout(() => {
             createPromotion(e)
-        }, i * 1000);
+        }, f * 1000);//tiempo de intervalo para la función
     }
 }
 function createPromotion (e) {
@@ -56,95 +86,73 @@ function createPromotion (e) {
     let dateFinal = Math.floor(dateNr/1000);
     createGiftcard(e,dateFinal)
 }
-async function obtenerCategorías (e) {
-    await api
-        .get(`${urlProducts}${e}.json?login=${login}&authtoken=${token}`)
-        .then( res => {
-      // handle success
-    const data = res.data.product.categories
-    const productName = res.data.product.name
-    const idProduct = res.data.product.id
-    data.forEach(f => {
-        if(f.id == 1439748){
-            console.log(`el producto ${productName} si cuenta con la categoría de Giftcard`)
-            productsId.push(idProduct)
-            return
-        }
-        else{
-            console.log(`el producto ${productName} no cuenta con la categoría de Giftcard`)
-        }
-        });
-    })
-    .catch(function (error) {
-        console.log(error)
-    })
-}
 function createGiftcard (data,data2) {
-    console.log(data)
-    console.log(data2)
-    const estructura = {
-        "promotion": {
-            "name": "TestNuevo",
-            "enabled": true,
-            "discount_target": "order",
-            "buys_at_least": "price",
-            "condition_price": 0,
-            "condition_qty": 0,
-            "quantity_x": 0,
-            "type": "fix",
-            "discount_amount_fix": `${data.price}`,
-            "discount_amount_percent": 0,
-            "lasts": "none",
-            "begins_at": "string",
-            "expires_at": "string",
-            "max_times_used": 1,
-            "cumulative": false,
-            "customers": "string",
-            "categories": [
-                {
-                    "id": 0
+        for (let i = 0; i < data.qty; i++) { 
+            const estructuraRep = {
+                "promotion": {
+                    "name": `Giftcard-${data2}-${i}`,
+                    "enabled": true,
+                    "discount_target": "order",
+                    "buys_at_least": "price",
+                    "condition_price": 0,
+                    "condition_qty": 0,
+                    "quantity_x": 0,
+                    "type": "fix",
+                    "discount_amount_fix": `${data.price}`,
+                    "discount_amount_percent": 0,
+                    "lasts": "none",
+                    "begins_at": "string",
+                    "expires_at": "string",
+                    "max_times_used": 1,
+                    "cumulative": false,
+                    "customers": "string",
+                    "categories": [
+                        {
+                            "id": 0
+                        }
+                    ],
+                    "customer_categories": [
+                        {
+                            "id": 0
+                        }
+                    ],
+                    "products": [
+                        {
+                            "id": 0
+                        }
+                    ],
+                    "products_x": [
+                        {
+                            "id": 0
+                        }
+                    ],
+                    "coupons": [
+                        {
+                            "code": `${data2}-${i}`,
+                            "usage_limit": 1
+                        }
+                    ],
+                    "countries": [
+                        "string"
+                    ],
+                    "regions": [
+                        "string"
+                    ],
+                    "municipalities": [
+                        "string"
+                    ]
                 }
-            ],
-            "customer_categories": [
-                {
-                    "id": 0
-                }
-            ],
-            "products": [
-                {
-                    "id": 0
-                }
-            ],
-            "products_x": [
-                {
-                    "id": 0
-                }
-            ],
-            "coupons": [
-                {
-                    "code": `${data2}`,
-                    "usage_limit": 1
-                }
-            ],
-            "countries": [
-                "string"
-            ],
-            "regions": [
-                "string"
-            ],
-            "municipalities": [
-                "string"
-            ]
-        }
-    }
-    api
-        .post(`${urlPromotions}?login=${login}&authtoken=${token}`,estructura)
-        .then(res =>{
-            console.log("funcionó")
-        })
-        .catch(err => {
-            if(err.response.status == 400){
-                console.log("Numero repetido")
             }
-        })
+            api
+                .post(`${urlPromotions}?login=${login}&authtoken=${token}`,estructuraRep)
+                .then(res =>{
+                    console.log("funcionó")
+                    Giftcard.push(`${data2}-${i}`)
+                })
+                .catch(err => {
+                    if(err.response.status == 400){
+                        console.log("Numero repetido")
+                    }
+                })
+        }
 }
