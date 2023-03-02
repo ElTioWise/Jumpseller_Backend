7c9b2b499a0a35b6bae753ecba578df008f2f470
ignacio <ignaciog@fixlabs.cl>
2023-03-02 12:22:23 -0300

    cambios en el módulo

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,6 +1,6 @@
 const {handleHttpError} = require ("../utils/handleError")
 const api = require('axios');
-const nodemailer = require('nodemailer');
+const enviarMail = require ("../utils/sendEmail")
 
 const login = process.env.LOGIN;
 const token = process.env.TOKEN;
@@ -150,35 +150,6 @@ function createGiftcard (data,data2) {
                 }
             })
     }
-}
-
-//Enviar mail
-enviarMail = async (data)=> {
-    const config = {
-        host : 'smtp.gmail.com',
-        port : 587,
-        auth : {
-            user : 'wiseflame2016@gmail.com',
-            pass : process.env.GMAIL
-        }
-    }
-    const mensaje = {
-        from : 'wiseflame2016@gmail.com',
-        to : 'wiseflame2016@gmail.com',
-        subject : 'Correo de pruebas',
-        text : `Envío de correo desde node js utilizando nodemailer, giftcard creadas : ${data}`,
-        html : `<ul><li>${data}</li></ul><img src="cid:unique@nodemailer.com"/>`,
-        attachments: [
-            {
-                filename: 'test.png',
-                path: './src/images/21_1800x1800.webp',
-                cid: 'unique@nodemailer.com'
-            }
-        ]
-    }
-    const transport = nodemailer.createTransport(config)
-    const info =  await transport.sendMail(mensaje);
-    console.log(info)
 }
 
 module.exports = { createOrder }
